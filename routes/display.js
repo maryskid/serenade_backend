dacb43914b56ed3055c8529dcfe6a74d198d9200
Xiaohuan <cao_xiaohuan@hotmail.fr>
2023-05-23 15:25:48 +0200

    second commit mymatcheswholikesme

--- routes/display.js
+++ routes/display.js
@@ -4,10 +4,9 @@ const User = require("../models/User");
 
 
 
-router.get('/wholikesme', async (req, res) => {
+router.post('/wholikesme', async (req, res) => {
     try {
         const {userToken} = req.body 
-  
       // Recherche l'utilisateur  en utilisant le token
       const user = await User.findOne({ token: userToken });
   
@@ -17,8 +16,6 @@ router.get('/wholikesme', async (req, res) => {
   
       // Recherche tous les utilisateurs qui ont aimé l'utilisateur 
       const usersWhoLikeMe = await User.find({ whoLikesMe: user});
-
-
   
       res.json({ result: true, data: usersWhoLikeMe});
     } catch (error) {
@@ -29,7 +26,7 @@ router.get('/wholikesme', async (req, res) => {
 
 
 
-  router.get('/mymatches', async (req, res) => {
+  router.post('/mymatches', async (req, res) => {
     try {
         const {userToken} = req.body
   
@@ -45,16 +42,10 @@ router.get('/wholikesme', async (req, res) => {
         _id: { $ne: user._id }, // $ne : Exclure l'utilisateur 
         myLikes: user._id, // // Les utilisateurs dont l'ID est présent dans myLikes
         whoLikesMe: user._id // Les utilisateurs dont l'ID de l'utilisateur est présent dans whoLikesMe
-      }).select('_id'); //pour spécifier que seuls les IDs des utilisateurs correspondants doivent être renvoyés dans le résultat
+      }); 
 
-      //extraire les IDs correspondants à partir des objets matches à l'aide de la méthode map.
-      const matchIds = matches.map(match => match._id);
-
-     // Récupérer les utilisateurs correspondants avec les IDs trouvés
-    const matchingUsers = await User.find({ _id: { $in: matchIds } });
-  
     // La réponse JSON renvoie un tableau contenant les utilisateurs correspondants.
-      res.json({ result: true, data:matchingUsers });
+      res.json({ result: true, data:matches });
     } catch (error) {
       return res.status(400).json({ result: false, message: error.message });
     }
