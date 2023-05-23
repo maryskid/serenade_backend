0b6a89ceb5c0de8b5429054170abee65e5bdf137
Xiaohuan <cao_xiaohuan@hotmail.fr>
2023-05-23 17:22:37 +0200

    third commit

--- routes/display.js
+++ routes/display.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models/User");
+const Match = require("../models/Match");
 
 
 
@@ -26,31 +27,35 @@ router.post('/wholikesme', async (req, res) => {
 
 
 
+
+  
   router.post('/mymatches', async (req, res) => {
     try {
-        const {userToken} = req.body
+      const { userToken } = req.body;
   
-      // Rechercher l'utilisateur actuel en utilisant le token
+      // Recherche l'utilisateur actuel en utilisant le token
       const user = await User.findOne({ token: userToken });
   
       if (!user) {
         return res.status(404).json({ result: false, message: 'User not found' });
       }
   
-      // Rechercher les correspondances entre myLikes et whoLikesMe
-      const matches = await User.find({
-        _id: { $ne: user._id }, // $ne : Exclure l'utilisateur 
-        myLikes: user._id, // // Les utilisateurs dont l'ID est présent dans myLikes
-        whoLikesMe: user._id // Les utilisateurs dont l'ID de l'utilisateur est présent dans whoLikesMe
-      }); 
-
-    // La réponse JSON renvoie un tableau contenant les utilisateurs correspondants.
-      res.json({ result: true, data:matches });
+      // Recherche les documents "Match" correspondants pour l'utilisateur
+      const matches = await Match.find({ user: user._id });
+  
+      // Récupére les IDs des utilisateurs qui ont matché 
+      const matchedUserIds = matches.map(match => match.matchedUser);
+  
+      // Recherche les utilisateurs correspondants dans la collection "User"
+      const matchedUsers = await User.find({ _id: { $in: matchedUserIds } });
+  
+      //  Donne les utilisateurs qui ont matché avec le user
+      res.json({ result: true, data: matchedUsers });
     } catch (error) {
       return res.status(400).json({ result: false, message: error.message });
     }
   });
   
-
+  
 
 module.exports = router;
