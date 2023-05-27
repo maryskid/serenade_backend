e4af00b1dacc4bce9944348cdd45c0ab6d688608
florent <lan.ngongang@gmail.com>
2023-05-27 18:42:56 +0200

    add uniqueness constraint to email and imaginaryName

--- models/User.js
+++ models/User.js
@@ -1,56 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const locationSchema = mongoose.Schema({
-    longitude: Number,
-    latitude: Number,
-    city: String,
+  longitude: Number,
+  latitude: Number,
+  city: String,
 });
 
 const userSearchSchema = mongoose.Schema({
-    maxDistance: Number,
-    ageMin: Number,
-    ageMax: Number,
-    genderLiked: String,
-    sexualityLiked: String,
+  maxDistance: Number,
+  ageMin: Number,
+  ageMax: Number,
+  genderLiked: String,
+  sexualityLiked: String,
 });
 
 const userSchema = mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      //unique: true
-    },
-    email: {
-      type: String,
-      required: true,
-      //unique: true
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    token: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    gender: String,
-    sexuality: String,
-    relationshipStatus: String,
-    birthdate: Date,
-    location: locationSchema,
-    pictures: [String],
-    isOnline: Boolean,
-    occupation: String,
-    description: String,
-    imaginaryName: String,
-    search: userSearchSchema,
-    whoLikesMe: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
-    myLikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
-    myDislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
-    myRelationships: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: String,
+  sexuality: String,
+  relationshipStatus: String,
+  birthdate: Date,
+  location: locationSchema,
+  pictures: [String],
+  isOnline: Boolean,
+  occupation: String,
+  description: String,
+  imaginaryName: { type: String, unique: true, required: true },
+  search: userSearchSchema,
+  whoLikesMe: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  myLikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  myDislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  myRelationships: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
 });
 
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
